c530b82d75c25def9f1c5783d9ad0dbff7a145a5
Ekram Hossain <ekramhossain117@gmail.com>
2023-05-21 00:12:50 +0600

    delete method added

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       res.send(result);
     })
 
+
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id=req.params.id
+      const query ={_id:new ObjectId(id)}
+      const result = await alltoyCollection.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
